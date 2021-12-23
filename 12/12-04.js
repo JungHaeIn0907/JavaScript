554ac57d6b6f0f774db72edc8b0c69a61c0eb4a6
JungHaeIn0907 <idc05132@naver.com>
2021-12-23 10:39:38 +0900

    message

---
+++ 12/12-04.js
@@ -0,0 +1,5 @@
+var f = function add(x,y) {
+  return x+y; 
+};
+
+console.log(add(2,y)); 
---
+++ 12/12-08.js
@@ -0,0 +1,6 @@
+function foo() {console.log('foo'); }
+
+foo(); 
+
+(function bar() {console.log('bar'); }); 
+bar(); 
